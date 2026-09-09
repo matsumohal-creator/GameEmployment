6300055544f1bc3284df008dda56a584a0a2c50c
matsumohal-creator <ma.tsu.mo.hal@gmail.com>
2026-09-09 12:40:52 +0900

    ブロック制作中
    未完成

--- Form Trace/Source/StageObject/Block/Block.cpp
+++ Form Trace/Source/StageObject/Block/Block.cpp
@@ -4,10 +4,54 @@
 
 void Block::Start()
 {
-	m_AABB = CollisionManager::GetInstance()->CreateAABB();
-	m_AABB->SetTargetPos(&m_Pos);
-	m_AABB->SetSize(VGet(1.0f, 1.0f, 1.0f));
-	m_AABB->SetLocalPos(VGet(0.0f, 0.5f, 0.0f));
+	int frameNum = MV1GetFrameNum(m_Handle);
+	printfDx("===== Block Model Frame Info =====\n");
+	printfDx("Frame Num : %d\n", frameNum);
+	
+	for (int i = 0; i < frameNum; i++) 
+	{
+		const char* name = MV1GetFrameName(m_Handle, i);
+
+		int result = MV1SetupReferenceMesh(m_Handle, i, TRUE);
+		
+		if (result != 0) 
+		{
+			printfDx(
+				"Frame[%d] : %s\n"
+				" Reference Mesh Setup Failed\n",
+				i,
+				name ? name : "NULL"
+			);
+			continue;
+		}
+
+		MV1_REF_POLYGONLIST refMesh = MV1GetReferenceMesh(m_Handle, i, TRUE);
+		VECTOR minPos = refMesh.MinPosition; 
+		VECTOR maxPos = refMesh.MaxPosition; 
+		VECTOR center = VGet(
+			(minPos.x + maxPos.x) * 0.5f,
+			(minPos.y + maxPos.y) * 0.5f,
+			(minPos.z + maxPos.z) * 0.5f
+		);
+		VECTOR size = VGet(
+			maxPos.x - minPos.x,
+			maxPos.y - minPos.y,
+			maxPos.z - minPos.z
+		
+		); 
+		CollisionAABB* aabb = CollisionManager::GetInstance()->CreateAABB(); 
+		
+		if (aabb) 
+		{ 
+			aabb->SetTargetPos(&m_Pos); 
+			aabb->SetLocalPos(center); 
+			aabb->SetSize(size);
+
+			m_AABBs.push_back(aabb); 
+		} 
+		printfDx("Frame[%d] : %s\n" " Type : AABB\n" " Center : (%f, %f, %f)\n" " Size : (%f, %f, %f)\n", i, name ? name : "NULL", center.x, center.y, center.z, size.x, size.y, size.z); 
+		MV1TerminateReferenceMesh(m_Handle, i, TRUE); 
+	} 
 }
 
 StageObject* Block::Clone()
@@ -16,6 +60,21 @@ StageObject* Block::Clone()
 
 	*clone = *this;
 	clone->m_Handle = MV1DuplicateModel(m_Handle);
+	clone->m_AABBs.clear();
+
+	for (CollisionAABB* aabb : m_AABBs) 
+	{
+		CollisionAABB* cloneAABB = CollisionManager::GetInstance()->CreateAABB();
+		if (!cloneAABB) 
+		{
+			continue;
+		}
+		
+		cloneAABB->SetTargetPos(&clone->m_Pos);
+		cloneAABB->SetLocalPos(aabb->GetLocalPos());
+		cloneAABB->SetSize(aabb->GetSize());
+		clone->m_AABBs.push_back(cloneAABB);
+	}
 
 	return clone;
 }
--- Form Trace/Source/StageObject/Block/Block.h
+++ Form Trace/Source/StageObject/Block/Block.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../StageObject.h"
+#include <vector>
 
 class CollisionAABB;
 
@@ -11,4 +12,12 @@ public:
 
 	void Start() override;
 	StageObject* Clone() override;
+
+	const std::vector<CollisionAABB*>& GetAABBs() const 
+	{ 
+		return m_AABBs;
+	}
+
+private:
+	std::vector<CollisionAABB*> m_AABBs;
 };
